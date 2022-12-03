1122431875999a186a32280e9d57ce8673f62c89
Raushan Kumar Jha <jha.rau@northeastern.edu>
2022-12-02 22:30:17 -0500

    Crested configuration for mongoose

---
+++ GetGrocery/backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app=express();
+const errorMiddleware= require("./middleware/error")
+const cookieParser=require("cookie-parser")
+
+app.use(express.json());
+app.use(cookieParser());
+
+//route imports
+
+const product=require("./routes/productRoute");
+const user=require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+
+app.use("/api/v1",product);
+app.use("/api/v1", order);
+app.use("/api/v1",user);
+
+
+//middleware for error
+app.use(errorMiddleware);
+
+
+module.exports=app 
